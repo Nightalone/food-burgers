0838332be9cbbc66775080e67da854f28c0094cf
Dyego Castro <dcastrocerqueira33@gmail.com>
2023-10-18 22:32:10 -0300

    'Up'

--- src/components/Drinks.jsx
+++ src/components/Drinks.jsx
@@ -13,73 +13,71 @@ function Drinks(){
         document.querySelector('#itens').classList.remove('limpar1');
         document.querySelector('#searc').classList.remove('limpar2');
         document.querySelector("#navegar").style.height = '300px'
-        document.querySelector("#navegar").style.transition = '1s'
         setAparecer('true')
       }else{
         document.querySelector('#itens').classList.add('limpar1');
         document.querySelector('#searc').classList.add('limpar2');
         document.querySelector("#navegar").style.height = '140px'
-        document.querySelector("#navegar").style.transition = '1s'
         setAparecer('false')
       }
     })
   }
 
   function pesquisar(){
-    var valor = document.querySelector('#produto').value.toLowerCase();
-    if(valor === 'strawberry juice'){
+    let valor = document.querySelector('#produto').value.toLowerCase();
+    if(valor === 'cheeseburger'){
       document.querySelector('#prot2').style.display = 'none';
       document.querySelector('#prot3').style.display = 'none';
       document.querySelector('#container2').style.display = 'none';
-      document.querySelector('nav').style.marginBottom = '10em'
-      document.querySelector('section').style.height = '10em'
-    }else if(valor === 'orange juice'){
+      document.querySelector('nav').classList.add('nav-small');
+      document.querySelector('nav').classList.remove('nav-mid');
+    }else if(valor === 'x-bacon'){
       document.querySelector('#prot1').style.display = 'none';
       document.querySelector('#prot3').style.display = 'none';
       document.querySelector('#container2').style.display = 'none';
-      document.querySelector('nav').style.marginBottom = '10em'
-      document.querySelector('section').style.height = '10em'
-    }else if(valor === 'pineapple juice'){
+      document.querySelector('nav').classList.add('nav-small');
+      document.querySelector('nav').classList.remove('nav-mid');
+    }else if(valor === 'x-salad'){
       document.querySelector('#prot1').style.display = 'none';
       document.querySelector('#prot2').style.display = 'none';
       document.querySelector('#container2').style.display = 'none';
-      document.querySelector('nav').style.marginBottom = '10em'
-      document.querySelector('section').style.height = '10em'
-    }else if(valor === 'coca cola'){
+      document.querySelector('nav').classList.add('nav-small');
+      document.querySelector('nav').classList.remove('nav-mid');
+    }else if(valor === 'chicken'){
       document.querySelector('#prot5').style.display = 'none';
       document.querySelector('#prot6').style.display = 'none';
       document.querySelector('#container1').style.display = 'none';
-      document.querySelector('nav').style.marginBottom = '10em'
-      document.querySelector('section').style.height = '10em'
-    }else if(valor === 'guarana'){
+      document.querySelector('nav').classList.add('nav-small');
+      document.querySelector('nav').classList.remove('nav-mid');
+    }else if(valor === 'double cheddar'){
       document.querySelector('#prot4').style.display = 'none';
       document.querySelector('#prot6').style.display = 'none';
       document.querySelector('#container1').style.display = 'none';
-      document.querySelector('nav').style.marginBottom = '10em'
-      document.querySelector('section').style.height = '10em'
-    }else if(valor === 'fanta'){
+      document.querySelector('nav').classList.add('nav-small');
+      document.querySelector('nav').classList.remove('nav-mid');
+    }else if(valor === 'x-everything'){
       document.querySelector('#prot4').style.display = 'none';
       document.querySelector('#prot5').style.display = 'none';
       document.querySelector('#container1').style.display = 'none';
-      document.querySelector('nav').style.marginBottom = '10em'
-      document.querySelector('section').style.height = '10em'
+      document.querySelector('nav').classList.add('nav-small');
+      document.querySelector('nav').classList.remove('nav-mid');
     }else{
-      document.querySelector('#prot1').style.display = 'block';
-      document.querySelector('#prot2').style.display = 'block'
-      document.querySelector('#prot3').style.display = 'block'
-      document.querySelector('#prot4').style.display = 'block'
-      document.querySelector('#prot5').style.display = 'block'
-      document.querySelector('#prot6').style.display = 'block'
+      document.querySelector('#prot2').style.display = 'block';
+      document.querySelector('#prot3').style.display = 'block';
+      document.querySelector('#prot4').style.display = 'block';
+      document.querySelector('#prot5').style.display = 'block';
+      document.querySelector('#prot6').style.display = 'block';
       document.querySelector('#container1').style.display = 'flex';
       document.querySelector('#container2').style.display = 'flex';
-      document.querySelector('nav').style.marginBottom = '38em'
-      document.querySelector('section').style.height = '34.8rem'
+      document.querySelector('nav').classList.remove('nav-small');
+      document.querySelector('nav').classList.add('nav-mid');
+      
     }
   }
 
   return(
     <main>
-      <nav id='navegar'>
+      <nav id='navegar' className='nav-mid'>
         <h1><span class="material-symbols-outlined">fastfood</span>Food Burgers</h1>
         <div className='container-menu' id='containe-menu' onClick={clickMenu}><span class="material-symbols-outlined menu">menu</span></div>
         <ul className='limpar1' id='itens'>
